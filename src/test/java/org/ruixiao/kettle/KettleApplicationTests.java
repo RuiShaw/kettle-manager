4904da07cec15ea34900740d3509d7193901f20d
Rui Shaw <Xiaorui0629@gmail.com>
2020-05-14 19:42:51 +0800

    initial project

---
+++ src/test/java/org/ruixiao/kettle/KettleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ruixiao.kettle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KettleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
